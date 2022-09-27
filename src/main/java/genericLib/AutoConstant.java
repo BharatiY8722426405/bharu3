171728033271763a99bac24ef52d3d2a0f20d7ac
Bharatiy8722426405 <bharatiyaligar1996@gmail.com>
2022-09-27 19:17:51 +0530

    second commit with edit

--- src/main/java/genericLib/AutoConstant.java
+++ src/main/java/genericLib/AutoConstant.java
@@ -4,4 +4,7 @@ public interface AutoConstant {
 
 	String photopath="./photo/";
 	String propertyfilePath="./src/test/resources/data.properties";
+	
+	
+	https://github.com/BharatiY8722426405/bharu3.git
 }
